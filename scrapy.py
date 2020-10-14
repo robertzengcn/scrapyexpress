27b6b25f5856e0152c96e904090c7c83ef213bcb
Robert Zeng <jianze_zeng@foxitsoftware.cn>
2020-10-14 22:16:36 +0800

    update ignore

--- scrapy.py
+++ scrapy.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 class Scrapy(object):
 
     def __init__(self):
@@ -33,9 +34,13 @@ class Scrapy(object):
         
         self.checkadexist()
         self.closeadultad()
+        self.browser.implicitly_wait(2)
         self.scrolldown()
-        self.browser.implicitly_wait(2)#等待页面加载
+        # self.browser.implicitly_wait(2)#等待页面加载
+        # self.scrolldown()
+        
         pagenum=self.getpagenum() #获取页面数量
+        print(pagenum)
         if pagenum>self.pagenum:
             self.pagenum=pagenum
         
@@ -78,11 +83,22 @@ class Scrapy(object):
         iamold.click() 
     # 获取总的页数    
     def getpagenum(self):
-
-        #totalpage=self.browser.find_element_by_class_name('total-page')
+        #先滚动到页面中间
+        # self.scrollcenter()
+        # self.browser.implicitly_wait(2)#等待页面加载
+        # self.scrolldown()#再滚动到页面底部
+        # self.scrollcenter()
+        self.scrolldownpage()
+        p4p = self.browser.find_element_by_id("p4p")
+        actions = ActionChains(self.browser)
+        actions.move_to_element(p4p).perform()
+        totalpage=self.browser.find_element_by_class_name('total-page')
         totalpage = WebDriverWait(self.browser, 10).until(
             EC.visibility_of_element_located((By.CLASS_NAME, 'total-page'))
             )
+        
+        # p4p = self.browser.find_element_by_id("")
+        # self.browser.execute_script("arguments[0].scrollIntoView(true);", element)
         inner_text= self.browser.execute_script("return arguments[0].innerText;", totalpage)
         # number=int(filter(inner_text.isdigit, inner_text))
         numarr=[int(s) for s in inner_text.split() if s.isdigit()]  
@@ -112,8 +128,29 @@ class Scrapy(object):
             print("not find 20201008124081")
             return False
         uinextad.click()
-    '''
-    滚动到页面底部
-    '''    
+
+    #滚动到页面底部   
     def scrolldown(self):
-        self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")    
+        self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")  
+    #滚动到页面中间
+    def scrollcenter(self):
+         self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight/2);") 
+    def scrolldownpage(self):
+        
+        # winsize=self.browser.get_window_size()
+        # print(winsize)
+        # winheight=winsize.get('height')
+        winheight = self.browser.execute_script("return document.body.scrollHeight")
+        print(winheight)
+        wlist=list(range(1,10))
+        for i in wlist:
+            tarhig=i*0.1*winheight #目标高度
+            command="window.scrollTo(0, "+str(tarhig)+");"
+            print(command)
+            self.browser.execute_script(command)
+            self.browser.implicitly_wait(3.5)#等待页面加载
+
+
+       
+
+           
